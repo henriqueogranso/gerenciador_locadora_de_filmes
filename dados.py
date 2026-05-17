d470d24b6073a6526d33ab10862987f13c300522
Henrique Granso <henrique.oliveira.granso@gmail.com>
2026-05-17 18:43:11 -0300

    novas implementações do gerenciador locadora de filmes: dados.py: filmes (lista), fila_reservas (FIFO), pilha_devolucoes (LIFO) estatus_opcoes (tupla) tarefas.py: adiciado listar_catalogo e historico_devolucoes, integrando fila e pilha nas funções cadastrar, reservar e devolução main.py: adicionado try/except, menu atualizado com mais 2 opções (8 opções agora no total) dados.py: adicionado filmes (lista), fila_reservas (FIFO), pilha_devolucoes (LIFO) e status_opcoes (tupla)

--- dados.py
+++ dados.py
@@ -5,5 +5,9 @@ titulo={'madagascar': {'genero': 'animação','ano':'2005','duracao': '1h30m', '
         ,'kung fu panda': {'genero': 'animação', 'ano': '2008', 'duracao': '1h30m', 'status': 'disponível'}
         }
 reservas = { 'pinguins':{ 'nome_cliente': 'joão', 'data_reserva': '01/01/2024', 'data_vencimento': '07/01/2024',}}
-
+# H fiz esses de baixo
+filmes = ['madagascar', 'pinguins', 'kung fu panda']
+fila_reservas = ['madagascar', 'kung fu panda']
+pilha_devolucoes = []
+status_opcoes = ('disponível', 'reservado', 'indisponível')
 # lista de reservas de filmes.
