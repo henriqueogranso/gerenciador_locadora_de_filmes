240d1d3623974ebd1be93a4bdcbdbe17f0c71f6e
VMansur98 <152087535+VMansur98@users.noreply.github.com>
2026-05-18 14:02:19 -0300

    versão 1.6.3

--- dados.py
+++ dados.py
@@ -7,11 +7,8 @@ titulo={'madagascar': {'genero': 'animação','ano':2005,'duracao': '1h30m', 'st
 reservas = { 'pinguins':{ 'nome_cliente': 'joão', 'data_reserva': '01/01/2024', 'data_vencimento': '07/01/2024',}}
 # H fiz esses de baixo
 filmes = ['madagascar', 'pinguins', 'kung fu panda']
-<<<<<<< HEAD
 fila_reservas = ['madagascar', 'kung fu panda']
-=======
 fila_reservas = [f for f in filmes if titulo[f]['status'] == 'disponível']
->>>>>>> teste_sistema
 pilha_devolucoes = []
 status_opcoes = ('disponível', 'reservado', 'indisponível')
 # lista de reservas de filmes.
